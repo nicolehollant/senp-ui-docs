3cb9000a8013dfe81b37e016c8ac8d8bc7dc5a39
Nicole Hollant <nicolehollant@gmail.com>
2023-01-06 15:55:22 -0500

    asdf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
     'vite:extendConfig': (config, { isClient, isServer }) => {
       if (isClient && config?.resolve?.alias) {
         ;(config.resolve.alias as any).vue = 'vue/dist/vue.esm-bundler'
+        ;(config.resolve.alias as any)['lodash.debounce'] = 'lodash.debounce/index.js'
+        ;(config.resolve.alias as any).vuedraggable = 'vuedraggable/dist/vuedraggable.common.js'
+        ;(config.resolve.alias as any).jsurl = 'jsurl/lib/jsurl.js'
       }
     },
   },
